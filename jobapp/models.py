bb4a8a81c7fb6d4ec787773dac6b9f4532b2f7f8
ShegosThato <cjmashego66@gmail.com>
2021-07-24 22:40:59 +0200

    Aded verbose name and plurals to category models

--- jobapp/models.py
+++ jobapp/models.py
@@ -17,6 +17,10 @@ JOB_TYPE = (
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
     
